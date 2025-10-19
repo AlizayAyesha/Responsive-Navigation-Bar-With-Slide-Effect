132a976d0adecded2894fa1adeec150c4912e01a
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-10-19 23:07:46 +0500

    feat: add responsive layout for the page (#11)

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
-    $(document).ready(function () {
-        // Toggle hamburger menu and slide toggle menu
-        $(".hamburger-menu").click(function () {
-            $(this).toggleClass("open");
-            $(".menu").slideToggle();
-        });
-    
-        // Add an active class in <a> tag for menu items
-        $(".menu li a").click(function () {
-            $(".menu li a").removeClass("active");
-            $(this).addClass("active");
-        });
+    // Toggle hamburger menu and slide toggle menu (mobile)
+    $(".hamburger-menu").on('click', function () {
+        var expanded = $(this).attr('aria-expanded') === 'true';
+        $(this).attr('aria-expanded', !expanded);
+        $(this).toggleClass("open");
+        $(".menu").stop(true, true).slideToggle(200);
     });
-    
+
+    // Add an active class in <a> tag for menu items
+    $(".menu li a").on('click', function () {
+        $(".menu li a").removeClass("active");
+        $(this).addClass("active");
+        // close menu after selecting a link
+        $(".hamburger-menu").removeClass('open').attr('aria-expanded', 'false');
+        $(".menu").slideUp(150);
+    });
+
     // Parallax effect on background image
     $(window).on("scroll", function() {
         var scrollTop = $(this).scrollTop();
